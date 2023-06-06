65213ea07f51e921fa7ec9de69b4cf53bf5d1e1c
SebastianSkowron <sebastian.skowron.dev@gmail.com>
2023-06-07 00:05:12 +0200

    main structure is done; routes are mostly done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Socials from "./Socials";
+import Logo from '../images/header/logo.svg';
+import MobileNav from "./MobileNav";
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+            <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
+                <Link to={'/'} className='max-w-[200px]'>
+                    <img src={Logo} alt='' />
+                </Link>
+                <nav className='hidden xl:flex gap-x-12 font-semibold'>
+                    <Link to={'/'} className='text-[#696C6D] hover:text-primary transistion'>
+                        Home
+                    </Link>
+                    <Link to={'/about'} className='text-[#696C6D] hover:text-primary transistion'>
+                        About
+                    </Link>
+                    <Link to={'/portfolio'} className='text-[#696C6D] hover:text-primary transistion'>
+                        Portfolio
+                    </Link>
+                    <Link to={'/contact'} className='text-[#696C6D] hover:text-primary transistion'>
+                        Contact
+                    </Link>
+                </nav>
+            </div>
+            <Socials />
+            <MobileNav />
+        </header>
+    )
 }
 
 export default Header
